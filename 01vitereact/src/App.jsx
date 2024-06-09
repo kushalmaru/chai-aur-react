efb251ad5ccdaeb4fce63e2054f183e092f6d06d
Kushal Maru <kushalmaru28@gmail.com>
2024-06-09 18:37:02 +0530

    importance of naming and .jsx

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,8 +1,13 @@
+import Chai from "./Chai"
 
 function App() {
   
   return (
-    <h1> Chai aur react with Vite</h1>
+    <>
+    <Chai/>
+    <h1>Chai aur react</h1>
+    <p>test para</p>
+    </>
   )
 }
 
